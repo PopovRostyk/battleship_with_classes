f304b9c9508af8bc812faee668898337f5c37951
dfghjm <popov@ucu.edu.ua>
2017-02-19 00:10:59 +0200

    _

--- classes.py
+++ classes.py
@@ -1,16 +1,5 @@
 import string
 
-class Ship:
-    def __init__(self, bow, horizontal, length, hit):
-        self.bow = bow
-        self.horizontal = horizontal
-        self.length = length
-        self._hit = hit
-
-    def shoot_at(self, position):
-        pass
-
-
 class Player:
     def __init__(self, name):
         self._name = name
@@ -25,16 +14,47 @@ class Field:
 
     def shoot_at(self, tuple):
         pass
-    @staticmethod
 
-    def field_without_ships():
-        lst = [string.ascii_lowercase[i] for i in range(10)]
-        return lst
-
+    def field_without_ships(self):
+        a = []
+        i = 1
+        a.append(string.ascii_uppercase[:10])
+        print('   ', end='')
+        for items in a:
+            for item in items:
+                print(item, end=' ')
+        print(a)
+        print('\n')
+        print('  ', '___________________')
+        b = [['X' for i in range(10)] for i in range(10)]
+        print(b)
+        for i in range(len(b)):
+            if i == 9:
+                print(i + 1, end=' ')
+            else:
+                print(i + 1, end='  ')
+            for items in b[i]:
+                for item in items:
+                    print(item, end=' ')
+            print('\n')
+        return b
     def field_with_ships(self):
         lst = [[' ' for i in range(10)] for i in range(10)]
         for items in lst:
             print(items)
+
+field = Field().field_without_ships()
+class Ship:
+    def __init__(self, bow=0, horizontal=0, length=0, hit=0):
+        self.bow = bow
+        self.horizontal = horizontal
+        self.length = length
+        self._hit = hit
+
+    def shoot_at(self, position):
+        pass
+
+
 
 
 class Game:
@@ -43,3 +63,4 @@ class Game:
         self._fields = fields
         self._players = players
 
+print(Ship().shoot_at((1, 'b')))
